448950da9858e7f20bbf2ee3a95be1d2aa8ad9f1
kstrauch94 <kstrauch0694@gmail.com>
2018-06-10 21:28:32 +0200

    actually revert changes to pbs_generic.sh

--- templates/pbs-generic.sh
+++ templates/pbs-generic.sh
@@ -11,7 +11,8 @@ function run()
 	# does not work because this will only kill mpiexec
 	trap abort SIGUSR1
 	/usr/bin/time -f "Real time (s): %e" -o runsolver.watcher \
-	{run.command} {run.file} {run.options} \
+	"{run.root}/programs/{run.solver}" {run.args} \
+	-f "{run.file}" \
 	> runsolver.solver 2>&1 &
 	export MPI_PID=$!
 	export BASH_PID=$$
@@ -23,11 +24,10 @@ function run()
 
 function run2()
 {{
-    echo PBS_NODEFILE $PBS_NODEFILE
-    echo PBS_NODES $PBS_NODES
-    MPIEXEC_TIMEOUT={run.timeout} \
+	MPIEXEC_TIMEOUT={run.timeout} \
 		mpiexec -machinefile $PBS_NODEFILE -n $PBS_NODES \
-         {run.command} {run.file} {run.options} \ 
+		"{run.root}/programs/{run.solver}" {run.args} \
+		-f instance \
 		> runsolver.solver 2>&1
 }}
 
@@ -40,9 +40,8 @@ case $1 in
 		run2
 		;;
 	*)    
-        cd "$(dirname $0)"
-		#zcat "{run.file}" > instance
-		cat "{run.file}" > instance
+		cd "$(dirname $0)"
+		zcat "{run.file}" > instance
 		[[ -e .finished ]] || /usr/bin/time -f "Real time (s): %e" -o runsolver.watcher "./$(basename $0)" run2
 		touch .finished
 		rm -f instance
