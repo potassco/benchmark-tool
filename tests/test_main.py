80fa8786aceeaabd364939e8a027d3a05ac174ff
tzschmidt <tzschmidt@gmx.de>
2025-04-15 14:20:08 +0200

    Setup tests dir

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+"""
+Test cases for main application functionality.
+"""
+
+from io import StringIO
+from unittest import TestCase
+from benchmarktool.result import ods_gen
+
+class TestMain(TestCase):
+    """
+    Test cases for main application functionality.
+    """
+
+    def test_test(self) -> None:
+        """
+        Test test.
+        """
+        pass
+
+
