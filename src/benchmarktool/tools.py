d8f968b85b4790f9de4d094dbacdddb4a8ac969c
tzschmidt <tzschmidt@gmx.de>
2025-05-20 15:23:15 +0200

    Clean-up tools

--- src/benchmarktool/tools.py
+++ src/benchmarktool/tools.py
@@ -5,10 +5,7 @@ Created on Jan 15, 2010
 """
 
 import os
-import random
 import stat
-from collections.abc import MutableSequence
-from typing import Any
 
 
 def mkdir_p(path: str) -> None:
@@ -52,86 +49,6 @@ def pbs_time(int_rep: int) -> str:
     int_rep //= 60
     h = int_rep
     return "{0:02}:{1:02}:{2:02}".format(h, m, s)
-
-
-def median_sorted(sequence: MutableSequence[Any]) -> Any:
-    """
-    Returns the median of a sorted sequence.
-    (Returns 0 if the sequence is empty.)
-
-    Attributes:
-        sequence (MutableSequence[Any]): Sequence
-    """
-    if len(sequence) == 0:
-        return 0
-    middle = len(sequence) // 2
-    value = sequence[middle]
-    if 2 * middle == len(sequence):
-        value = (value + sequence[middle - 1]) / 2.0
-    return value
-
-
-# unsused -> np.median
-# consider removing
-# pylint: disable=consider-using-max-builtin
-def median(sequence: MutableSequence[Any]) -> Any:
-    """
-    Returns the median of an unordered sequence.
-    (Returns 0 if the sequence is empty.)
-
-    Attributes:
-        sequence (MutableSequence[Any]): Sequence
-    """
-
-    def partition(sequence: MutableSequence[Any], left: int, right: int) -> int:
-        """
-        Selects a pivot element and moves all smaller(bigger)
-        elements to the left(right).
-
-        Attributes:
-            sequence (MutableSequence[Any]): Sequence
-            left (int):                      left index
-            right (int):                     right index
-        """
-        pivot_idx = random.randint(left, right)
-        pivot_value = sequence[pivot_idx]
-        sequence[pivot_idx], sequence[right] = sequence[right], sequence[pivot_idx]
-        store_idx = left
-        for i in range(left, right):
-            if sequence[i] < pivot_value:
-                sequence[store_idx], sequence[i] = sequence[i], sequence[store_idx]
-                store_idx = store_idx + 1
-        sequence[right], sequence[store_idx] = sequence[store_idx], sequence[right]
-        return store_idx
-
-    def select(sequence: MutableSequence[Any], left: int, right: int, k: int) -> Any:
-        """
-        Selects the k-th element as in the ordered sequence.
-
-        Attributes:
-            sequence (MutableSequence[Any]): Sequence
-            left (int):                      left index
-            right (int):                     right index
-        """
-        pivot_idx = partition(sequence, left, right)
-        if k == pivot_idx:
-            return sequence[k]
-        if k < pivot_idx:
-            return select(sequence, left, pivot_idx - 1, k)
-        return select(sequence, pivot_idx + 1, right, k)
-
-    if len(sequence) == 0:
-        return 0
-    middle = len(sequence) // 2
-    select(sequence, 0, len(sequence) - 1, middle)
-    value = sequence[middle]
-    if 2 * middle == len(sequence):
-        maximum = sequence[middle - 1]
-        for x in sequence[: middle - 1]:
-            if x > maximum:
-                maximum = x
-        value = (value + maximum) / 2.0
-    return value
 
 
 def set_executable(filename: str) -> None:
