1d3bb3a7f25b509b2370d64a52c3093b699751dd
tzschmidt <tzschmidt@gmx.de>
2025-07-03 14:11:43 +0200

    Improve verify_results script

--- verify_results.sh
+++ verify_results.sh
@@ -4,8 +4,9 @@
 help() {
     echo "This script can be used to check benchmark results for runlim errors and re-run such instances."
     echo "Make sure, the environment where the benchmark-tool is installed is active."
+    echo "Check the benchmark-tool documentation for more information"
     echo
-    echo "Syntax: ${0##*/} <runscript> [-h]"
+    echo "Syntax: ${0##*/} [-h] <runscript.xml>"
     echo "options:"
     echo "h    Print this Help."
     echo
@@ -16,17 +17,31 @@ while getopts ":h" option; do
     case $option in
     	h) # display Help
     		help
-    		exit;;
+    		exit 0
+            ;;
     	\?) # invalid option
     		echo "Error: Invalid option" >&2
-		exit;;
+		    exit 1
+            ;;
     esac
 done
+
+# invalid input
+if ! [ -f $1 ]; then
+    echo "Error: provided runscript doesn't exist" >&2
+    exit 1
+fi
 
 # search runlim erros
 echo "Look for runlim errors..."
 DIR=$(cat $1 | sed -nr 's/^.*output="(\S+)".*$/\1/p')
 readarray -d '' < <(find $DIR -name "runsolver.watcher" -exec grep -q "runlim error" {} \; -print0)
+
+# exit if nothing found
+if [ -z ${MAPFILE[@]} ]; then
+    echo "No runlim erros found"
+    exit 0
+fi
 
 # delete .finished
 for FILE in ${MAPFILE[@]}
@@ -44,4 +59,3 @@ if ! [ -x "$(command -v bgen)" ]; then
     exit 1
 fi
 bgen -e $1
-
