a297be27e7ee16aa457c515146ecde01b2de9c80
tzschmidt <tzschmidt@gmx.de>
2025-07-03 13:48:45 +0200

    Use real-time-limit

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -10,7 +10,7 @@ cd "$(dirname $0)"
 [[ -e .finished ]] || "{run.root}/programs/runlim-2.0.0rc12" \
 	--space-limit=20000 \
 	--output-file=runsolver.watcher \
-	--time-limit={run.timeout} \
+	--real-time-limit={run.timeout} \
 	"{run.root}/programs/{run.solver}" {run.args} "{run.file}" {run.encodings} > runsolver.solver
 
 touch .finished
