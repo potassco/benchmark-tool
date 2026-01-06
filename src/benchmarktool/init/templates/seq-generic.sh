66a94f9309b208a0f6b2310eaec5f821459e6197
tzschmidt <tzschmidt@gmx.de>
2026-01-06 15:18:40 +0100

    Simplify templates

--- src/benchmarktool/init/templates/seq-generic.sh
+++ src/benchmarktool/init/templates/seq-generic.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 # https://github.com/arminbiere/runlim
 
+CAT="{root}/programs/gcat.sh"
+
 cd "$(dirname $0)"
 
-[[ -e .finished ]] || "{run.root}/programs/runlim" \
-	--space-limit={run.memout} \
-	--output-file=runsolver.watcher \
-	--real-time-limit={run.timeout} \
-	"{run.root}/programs/{run.solver}" {run.args} {run.files} {run.encodings} > runsolver.solver
+runner=( "{root}/programs/runlim" \
+  {options} \
+  --space-limit={memout} \
+  --output-file=runsolver.watcher \
+  --real-time-limit={timeout} \
+  "{root}/programs/{solver}" {args})
+
+input=( {files} {encodings} )
+
+if [[ ! -e .finished ]]; then
+  {{
+    if file -b --mime-type -L  "${{input[@]}}" | grep -qv "text/"; then
+      "$CAT" "${{input[@]}}" | "${{runner[@]}}"
+    else
+      "${{runner[@]}}" "${{input[@]}}"
+    fi
+  }} > runsolver.solver
+fi
 
 touch .finished
